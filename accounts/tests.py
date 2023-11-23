b7f8e64fecd5d1bca63886fbd3dcc91c9bb62e88
simlexx-k <simonwriter254@gmail.com>
2023-11-23 23:15:05 +0300

    Third commit

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
+from .models import UserProfile
 # Create your tests here.
+
+class UserProfileTest(TestCase):
+    def test_something(self):
+        # Create a test instance of the User model
+        user = User.objects.create(username="testuser")
+
+        # Create a test instance of the UserProfile model
+        instance = UserProfile.objects.create(user=user,)
+        
+        # Perform assertions to check if the expected behavior is met
+        self.assertEqual(instance.user, user)
+    
